6720c0b64e86fc56032b19a78d4193a58cc07ea0
uis <uis.reg@yandex.ru>
2023-04-08 19:15:28 +0300

    change config

--- src/main/java/org/liceum/edm/config/MvcConfig.java
+++ src/main/java/org/liceum/edm/config/MvcConfig.java
@@ -1,13 +1,13 @@
 package org.liceum.edm.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-@Configuration
-public class MvcConfig implements WebMvcConfigurer{
-    public void addViewControllers(ViewControllerRegistry registry){
-        registry.addViewController("/login").setViewName("login");
-    }
-}
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//
+//@Configuration
+//public class MvcConfig implements WebMvcConfigurer{
+//    public void addViewControllers(ViewControllerRegistry registry){
+//        registry.addViewController("/login").setViewName("login");
+//    }
+//}
